b435ea52b46d40eec0679165e952cb621c5c10ab
aanshs7 <aanshs7@gmail.com>
2024-07-11 23:54:25 +0400

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ azure_openai_endpoint = os.environ.get("AZURE_OPENAI_ENDPOINT")
 azure_openai_key = os.environ.get("AZURE_OPENAI_KEY")
 openai_api_key = os.environ.get("OPENAI_API_KEY")
 authentication_required = str_to_bool(os.environ.get("AUTHENTICATION_REQUIRED", False))
-assistant_ids = {"ADQ assistant":os.environ.get("ASSISTANT_ID_ADQ"), 
+assistant_ids = {"ADQ assistant":os.environ.get("ASSISTANT_ID_CORP"), 
                  "know all assistant":os.environ.get("ASSISTANT_ID_knowall")}
 instructions = os.environ.get("RUN_INSTRUCTIONS", "")
 assistant_title = os.environ.get("ASSISTANT_TITLE", "Assistants API UI")
